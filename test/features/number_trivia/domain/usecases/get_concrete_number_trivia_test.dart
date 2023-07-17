73a933eeb0b5f450a4480bbcbb120caf35a93edb
ElTowa <ElTowa@NTPHAT>
2023-07-17 13:38:54 +0700

    C3.1 Refactor the "GetConcreteNumberTrivia" usecase to use callable classes

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -26,7 +26,7 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
           .thenAnswer((_) async => Right(tNumberTrivia));
       //act
-      final result = await usecase.execute(number: tNumber);
+      final result = await usecase(Params(number: tNumber));
       //assert
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
