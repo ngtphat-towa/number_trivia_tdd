8ad94015c1c083365a4a929a5211c3f90d1bf69f
ElTowa <ElTowa@NTPHAT>
2023-07-18 12:55:25 +0700

    C7.2 Rename core/platform to core/network

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -4,7 +4,7 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia_tdd/core/error/exception.dart';
 import 'package:number_trivia_tdd/core/error/failures.dart';
-import 'package:number_trivia_tdd/core/platform/network_info.dart';
+import 'package:number_trivia_tdd/core/network/network_info.dart';
 import 'package:number_trivia_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia_tdd/features/number_trivia/data/models/number_trivia_model.dart';
