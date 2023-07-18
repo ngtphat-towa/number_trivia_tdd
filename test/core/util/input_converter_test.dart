de90b2f09058d6fad04e480d2d90213370e7edd0
ElTowa <ElTowa@NTPHAT>
2023-07-18 19:09:34 +0700

    C10.2 Add Input Converter Tests

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia_tdd/core/util/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test(
+        'should return an integer when the string represents an unsigned integer',
+        () async {
+      //arrange
+      const str = '123';
+      //act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      //assert
+      expect(result, equals(Right(123)));
+    });
+
+    test('should return a Failure when the string is not an integer', () async {
+      //arrange
+      const str = 'abc';
+      //act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      //assert
+      expect(result, equals(Left(InvalidInputFailure())));
+    });
+
+    test('should return a Failure when the string is a negative integer',
+        () async {
+      //arrange
+      const str = '-123';
+      //act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      //assert
+      expect(result, equals(Left(InvalidInputFailure())));
+    });
+  });
+}
